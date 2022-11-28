ec87cd1c2f83ddf9763ed78ad6ef63ce535c51a6
Matsdj <mats2002.com@gmail.com>
2022-11-28 13:52:11 +0100

    Comments

--- Assets/Scripts/BuildingPreset.cs
+++ Assets/Scripts/BuildingPreset.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Building", menuName = "ScriptableObjects/Building", order = 1)]
 public class BuildingPreset : ScriptableObject
 {
+    /// <summary>The visual for the building</summary>
     public GameObject Model;
+    /// <summary>The resources that are removed when it is placed</summary>
     [TableList] public Resource[] BuildCost;
+    /// <summary>The resources that are made when it is placed</summary>
     [TableList] public Resource[] InitialProduction;
+    /// <summary>These resources are removed from its upkeep storage</summary>
     [TableList] public Resource[] Upkeep;
+    /// <summary>These resources are added to its production storage</summary>
     [TableList] public Resource[] Production;
     public int UpkeepStorageSize;
     public int ProductionStorageSize;
--- Assets/Scripts/Resources/ResourceManager.cs
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -6,12 +6,14 @@ public class ResourceManager : SingletonBehaviour<ResourceManager>
     public ResourceUI[] uiObjects;
     [TableList, SerializeField] private Resource[] resources;
 
+    /// <summary> Adds the resource to resources. If the resource is the same as People it also adds AvailablePeople</summary>
     public void AddResource(Resource resource)
     {
         resources[(int)resource.Type].Amount += resource.Amount;
         if (resource.Type == ResourceType.People) resources[(int)ResourceType.AvailablePeople].Amount += resource.Amount;
     }
 
+    /// <summary> Adds the resources to this.resources. If the resource is the same as People it also adds AvailablePeople</summary>
     public void AddResource(Resource[] resources)
     {
         for (int i = 0; i < resources.Length; i++)
@@ -20,12 +22,14 @@ public class ResourceManager : SingletonBehaviour<ResourceManager>
         }
     }
 
+    /// <summary> Removes the resource from resources. If the resource is the same as People it also removes AvailablePeople</summary>
     public void RemoveResource(Resource resource)
     {
         resources[(int)resource.Type].Amount -= resource.Amount;
         if (resource.Type == ResourceType.People) resources[(int)ResourceType.AvailablePeople].Amount -= resource.Amount;
     }
 
+    /// <summary> Removes the resource to resources. If the resource is the same as People it also adds AvailablePeople</summary>
     public void RemoveResource(Resource[] resources)
     {
         for (int i = 0; i < resources.Length; i++)
@@ -34,11 +38,15 @@ public class ResourceManager : SingletonBehaviour<ResourceManager>
         }
     }
 
+    /// <summary>Checks to see if there are more or equal resources to the amount of resources that is given</summary>
+    /// <returns>True if there are enough resources, false if there are not</returns>
     public bool CheckEnoughResources(Resource resource)
     {
         return Resource.CheckEnoughResources(resources, resource);
     }
 
+    /// <summary>Checks to see if there are more or equal resources to the amount of resources that is given</summary>
+    /// <returns>True if there are enough resources, false if there are not</returns>
     public bool CheckEnoughResources(Resource[] resources)
     {
         return Resource.CheckEnoughResources(this.resources, resources);
