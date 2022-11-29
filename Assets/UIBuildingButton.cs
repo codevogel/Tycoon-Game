87a6230ea07ec50cfac84eb89a5b3f780593c475
Quintin Yu <quintinyu02072@gmail.com>
2022-11-29 13:29:29 +0100

    Building UI basic

---
+++ Assets/UIBuildingButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBuildingButton : MonoBehaviour
+{
+    public void PressMe(string hello){
+        Debug.Log(hello);
+    }
+}
