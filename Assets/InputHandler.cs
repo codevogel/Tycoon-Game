9874f275205ed05d3756aaa2ebe677c32f4918c0
Thomas Kretzschmar <thomaskretzschmar5@gmail.com>
2022-12-02 15:25:25 +0100

    Camera rotation Input gemaakt

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -7,12 +7,25 @@ using UnityEngine.EventSystems;
 public class InputHandler : MonoBehaviour,
 IPointerUpHandler, IPointerDownHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
 {
+  #region WASD input variables
   public Camera sceneCamera;
-  #region input parameters
   private Vector3 _movementInput;
   private Vector3 _moveDirection;
   private Vector2 _mousePosition;
   #endregion
+
+  #region rotation variables 
+  private Vector3 _firstPoint;
+  private Vector3 _secondPoint;
+
+  private float _xAngle;
+  private float _yAngle;
+  private float _xAngTemp;
+  private float _yAngTemp;
+  private bool _isMidMouseDown = false;
+
+  #endregion
+
   #region Onpointer methods
   public void OnPointerClick(PointerEventData eventData)
   {
@@ -51,16 +64,41 @@ IPointerUpHandler, IPointerDownHandler, IPointerClickHandler, IPointerEnterHandl
 
   void Update()
   {
-    #region input
+    #region WASD Input
     _movementInput.x = Input.GetAxisRaw("Horizontal");
     _movementInput.z = Input.GetAxisRaw("Vertical");
     _moveDirection = new Vector3(_movementInput.x, 0f, _movementInput.z).normalized;//normalized so diagonal goes same speed
     _mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
     #endregion
+
+    #region rotation input
+    if (Input.GetMouseButtonDown(2))
+    {
+      _firstPoint = Input.mousePosition;
+      _xAngTemp = _xAngle;
+      _yAngTemp = _yAngle;
+      _isMidMouseDown = true;
+    }
+
+    if (Input.GetMouseButtonUp(2))
+    {
+      _isMidMouseDown = false;
+    }
+
+    if (_isMidMouseDown)
+    {
+      _secondPoint = Input.mousePosition;
+      _xAngle = _xAngTemp + (_secondPoint.x - _firstPoint.x) * 180.0f / Screen.width;
+      _yAngle = _yAngTemp - (_secondPoint.y - _firstPoint.y) * 90.0f / Screen.height;
+      transform.rotation = Quaternion.Euler(_yAngle, _xAngle, 0.0f);
+    }
+    #endregion
   }
 
   void FixedUpdate()
   {
+    #region WASD movement
     sceneCamera.transform.position += new Vector3(_moveDirection.x, 0f, _moveDirection.z);
+    #endregion
   }
 }
