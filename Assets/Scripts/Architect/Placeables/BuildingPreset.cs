5688225606060c866b6620d625162bfb382454bf
Matsdj <mats2002.com@gmail.com>
2022-12-08 11:52:06 +0100

    Succesfull Merge?

--- Assets/Scripts/Architect/Placeables/BuildingPreset.cs
+++ Assets/Scripts/Architect/Placeables/BuildingPreset.cs
@@ -3,13 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Grid/Buildings/BuildingPreset.cs
 [CreateAssetMenu(fileName = "Building", menuName = "ScriptableObjects/Buildings/Base", order = 1)]
-public class BuildingPreset : ScriptableObject
-========
-[CreateAssetMenu(fileName = "Building", menuName = "ScriptableObjects/Building", order = 1)]
 public class BuildingPreset : PlaceablePreset
->>>>>>>> Dev-Branch:Assets/Scripts/Architect/Placeables/BuildingPreset.cs
 {
     /// <summary>The resources that are made when it is placed</summary>
     [TableList] public Resource[] InitialProduction;
