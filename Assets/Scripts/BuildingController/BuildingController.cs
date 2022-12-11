f08d2ba7320fc003b25c74655c321d728e0f31a0
Kamiel <kamieldevisser@gmail.com>
2022-12-11 21:46:15 +0100

    remove buildings list from BuildingController

--- Assets/Scripts/BuildingController/BuildingController.cs
+++ Assets/Scripts/BuildingController/BuildingController.cs
@@ -5,22 +5,13 @@ using UnityEngine.Events;
 
 public class BuildingController : MonoBehaviour
 {
-    private static List<Building> buildings;
     public static int Tick { get; private set; }
 
     public static UnityEvent Produce;
     public static UnityEvent Transport;
-
-    public static void AddBuilding(Building building)
-    {
-        buildings.Add(building);
-    }
-
-
     public void FixedUpdate()
     {
         Tick++;
         Produce.Invoke();
     }
-
 }
