09cbd748a5bfdcf23a8a56bab5006786f4dd8e8f
Quintin Yu <quintinyu02072@gmail.com>
2022-12-21 15:03:46 +0100

    Show PopUp decoppled

--- Assets/Scripts/Architect/ArchitectController.cs
+++ Assets/Scripts/Architect/ArchitectController.cs
@@ -41,11 +41,6 @@ public class ArchitectController : SingletonBehaviour<ArchitectController>
 
     private BuildingPreset CurrentBuildingPreset => PlaceableBuildings[_currentBuildingIndex];
 
-    #region Popup vars
-    public GameObject popup;
-    public TMP_Text popuptext;
-    private Tile _popupTile;
-    #endregion
     // Update is called once per frame
     void Update()
     {
@@ -54,13 +49,6 @@ public class ArchitectController : SingletonBehaviour<ArchitectController>
         if (Input.GetMouseButton(0))
         {
             AttemptToPlaceObject();
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            ShowPopup();
-            //Move to button on popup
-            //AttemptToRemoveObject();
         }
 
         //alpha key 1 and 2 do not work?
@@ -125,8 +113,6 @@ public class ArchitectController : SingletonBehaviour<ArchitectController>
         };
     }
 
-
-
     /// <summary>
     /// Attempts to remove a building at the tile under the mouse.
     /// </summary>
@@ -161,55 +147,6 @@ public class ArchitectController : SingletonBehaviour<ArchitectController>
     private void SelectNextBuilding()
     {
         _currentBuildingIndex = (_currentBuildingIndex + 1) % PlaceableBuildings.Count;
-    }
-
-
-    /// <summary>
-    /// Removes the contents at targetTile.
-    /// </summary>
-    /// <param name="popupTile">The tile at which to remove the contents.</param>
-    public void RemoveObjectAt()
-    {
-        _popupTile.RemoveContent();
-        _popupTile.blockContentPlacement.gameObject.SetActive(false);
-        popup.SetActive(false);
-    }
-
-    /// <summary>
-    /// doet popup laten zien on right click. 
-    /// popup geeft informatie over de building/road en buttons om te upgraden/destroyen.
-    /// </summary>
-    void ShowPopup()
-    {
-        TileCoordinates coords = GridManager.Instance.GetTileCoordsFromMousePos();
-        _popupTile = GridManager.Instance.GetTileAt(coords.indices);
-        if (_popupTile == null) return;
-        if (_popupTile.HasContent)
-        {
-            if (_popupTile.Content is Building)
-            {
-                string output = "";
-                popup.SetActive(true);
-                Building currentBuilding = (Building)_popupTile.Content;
-                Debug.Log(currentBuilding.output.Contents.Count);
-                foreach (KeyValuePair<ResourceType, int> kvp in currentBuilding.output.Contents)
-                {
-                    output += string.Format("Resource = {0}, Amount  = {1}", kvp.Key, kvp.Value);
-                    output += "\n";
-                }
-
-                popuptext.text = $"{output}";
-                Console.WriteLine(output);
-            }
-            else if (_popupTile.Content is Road)
-            {
-
-            }
-        }
-        else
-        {
-            return;
-        }
     }
 
     public void SelectBuildingZero()
