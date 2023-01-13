d2b77e2d336bba8c9756d0246946f59e0cf4fde5
Laurens <Laurenscnossen@gmail.com>
2023-01-13 11:32:05 +0100

    agentspawner delay

--- Assets/BillboardText.cs
+++ Assets/BillboardText.cs
@@ -1,26 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BillboardText : MonoBehaviour
 {
     private Camera _camera;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (_camera != null)
         {
             var cameraTransform = _camera.transform;
             var position = cameraTransform.position;
             transform.LookAt(new Vector3(
-                x: position.x, 
+                x: position.x,
                 45,
                 z: position.z)
             );
