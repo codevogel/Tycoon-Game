3a6f04c0b8edb51ece86def1186bbfa3c3725951
Quintin Yu <quintinyu02072@gmail.com>
2023-01-18 11:46:26 +0100

    comment

--- Assets/Scripts/UI/KeepSelected.cs
+++ Assets/Scripts/UI/KeepSelected.cs
@@ -2,6 +2,9 @@
  using UnityEngine;
  using UnityEngine.UI;
 
+ /// <summary>
+ /// This class is made to keep a button opaque instead of transparent to make it clear this is the selected building
+ /// </summary>
  public class KeepSelected : MonoBehaviour
  {
      private Button ToKeepActive;
