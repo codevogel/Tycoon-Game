f02e062bd4dc5b79643265ae59e2fd41cd690cbe
Quintin Yu <36072659+Quintin-Yu@users.noreply.github.com>
2023-01-24 10:57:52 +0100

    Added Healthbar
    Gates now have healthbars;
    - Healhbar only goes down when attacked
    - Changed logic so when gate is damaged it checks the health
    - Added Billboard Script to healthbar so it faces the player at all times.

--- Assets/Scripts/Agency/TargetBehaviour.cs
+++ Assets/Scripts/Agency/TargetBehaviour.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Unity.UI;
+using UnityEngine.UI;
 
 namespace Agency
 {
@@ -6,32 +8,33 @@ namespace Agency
     {
         public int health;
         [SerializeField] private int baseHealth;
+
+        [SerializeField] private Slider healthBar;
         public float armor = 1; //armor stat sets how often a target can be attacked
         [SerializeField] ParticleSystem onDamage;
 
         private void Awake()
         {
             health = baseHealth;
-        }
-
-        private void Update()
-        {
-            Destroyed();
+            healthBar.value = (float)health / baseHealth;
         }
 
         private void Destroyed()
         {
-            if (health <= 0)
-            {
-                GameManager.Instance.GameOver();
-                gameObject.SetActive(false);
-            }
+            GameManager.Instance.GameOver();
+            gameObject.SetActive(false);
         }
 
         public void DoDamage(int damage)
         {
             health -= damage;
+            healthBar.value = (float)health / baseHealth;
             onDamage.Play();
+            
+            if (health <= 0)
+            {
+                Destroyed();
+            }
         }
     }
 }
